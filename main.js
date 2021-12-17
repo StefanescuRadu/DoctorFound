e8a28161aae2a1fbf7aa2335773e8ec3d786539b
Stefanescu Radu <radu98fm@gmail.com>
2021-12-17 11:11:44 +0200

    init map

--- main.js
+++ main.js
@@ -1,12 +1,24 @@
 let map;
-
+let position ;
 function initMap() {
     map = new google.maps.Map(document.getElementById("map"), {
         center: { lat: -34.397, lng: 150.644 },
-        zoom: 8,
+        zoom: 5,
     });
 }
+function getLocation() {
+    if (navigator.geolocation) {
+       position = navigator.geolocation.getCurrentPosition(showPosition);
+       console.log(position);
+    } else {
+        console.log("Geolocation is not supported by this browser.") ;
+    }
+}
 
+function showPosition(position) {
+    console.log("Latitude: " + position.coords.latitude +
+        "Longitude: " + position.coords.longitude)
+}
 initMap();
 
 // https://developers.google.com/maps/documentation/javascript/overview#maps_map_simple-css
